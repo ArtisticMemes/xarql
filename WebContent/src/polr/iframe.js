6b05b7abe8cd4e0cf13823e47c052530e0709419
ArtisticMemes <bryanchristopherjohnson@gmail.com>
2018-08-16 10:10:13 -0400

    Inject iframe From URL On Page Load

--- WebContent/src/polr/iframe.js
+++ WebContent/src/polr/iframe.js
@@ -1,4 +1,30 @@
 $(document).ready(function () {
+	function getUrlParameter(sParam) {
+	    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+	        sURLVariables = sPageURL.split('&'),
+	        sParameterName,
+	        i;
+
+	    for (i = 0; i < sURLVariables.length; i++) {
+	        sParameterName = sURLVariables[i].split('=');
+
+	        if (sParameterName[0] === sParam) {
+	            return sParameterName[1] === undefined ? true : sParameterName[1];
+	        }
+	    }
+	};
+	// Inject iframe mentioned in url
+	var urliframe = getUrlParameter('frame');
+	if(typeof urliframe != 'undefined') {
+		var iframe = document.createElement('iframe');
+		iframe.setAttribute('src', 'http://xarql.com/polr/peek?id=' + urliframe);
+		iframe.setAttribute('class', 'injected-iframe');
+		iframe.setAttribute('id', 'iframe-' + urliframe);
+		document.getElementById('wrapper').appendChild(iframe);
+		$('#injector' + urliframe).hide();
+		$('#remover' + urliframe).show();
+	}
+	
 	$('.inject-button').each(function () {
 		var $this = $(this);
 		$this.on("click", function () {
