f6780c07c78be4092e7c105d4c5b2cde8767d734
ArtisticMemes <bryanchristopherjohnson@gmail.com>
2018-12-15 14:54:30 -0500

    Accounts (Incomplete)

--- xarql.com/src/com/xarql/user/AccountPage.java
+++ xarql.com/src/com/xarql/user/AccountPage.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.xarql.util.BuildTimer;
+import com.xarql.util.ServletUtilities;
+
 /**
  * Servlet implementation class AccountPage
  */
@@ -32,7 +35,10 @@ public class AccountPage extends HttpServlet
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-
+        BuildTimer bt = new BuildTimer(request);
+        ServletUtilities util = new ServletUtilities(request);
+        util.standardSetup();
+        request.getRequestDispatcher("/src/user/login.jsp").forward(request, response);
     } // doGet()
 
     /**
@@ -42,7 +48,6 @@ public class AccountPage extends HttpServlet
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        // TODO Auto-generated method stub
         doGet(request, response);
     } // doPost()
 
