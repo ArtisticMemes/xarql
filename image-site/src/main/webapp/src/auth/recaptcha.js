349a40cb50ebea247ef3d4a0eb8342c5a67ec667
Bryan Christopher Johnson <bryanchristopherjohnson@gmail.com>
2020-01-22 16:56:39 -0500

    Use eslint as JS linter
    Tested and completed linting on auth.js of main-site and recaptcha.js of image-site

--- image-site/src/main/webapp/src/auth/recaptcha.js
+++ image-site/src/main/webapp/src/auth/recaptcha.js
@@ -1,38 +1,45 @@
-var domain = document.getElementById('domain').getAttribute('value');
-function recaptchaCallback()
-{
-	document.getElementById('recaptcha_check_empty').value = 1;
-	var recaptchaData = grecaptcha.getResponse();
-	var xhr = new XMLHttpRequest();
-	xhr.open('POST', domain + '/-/auth/recaptcha');
-	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	xhr.onload = function() {
-	  //console.log('response loaded');
-	};
-	xhr.send('data=' + recaptchaData);
-	var recaptchaWorked = setTimeout(checkStatusHidden(), 2000);
-	if(recaptchaWorked)
-	{
-		var toRemove = document.getElementById('recaptcha-form');
-	    toRemove.parentNode.removeChild(toRemove);
-	}
+/* global document, grecaptcha, XMLHttpRequest, console, setTimeout */
+/* exported recaptchaCallback */
+
+var HTTP_READY = 4;
+var HTTP_OK = 200;
+var HTTP_CLIENT_ERROR = 401;
+var RECAPTCHA_TIMEOUT = 2000; // in milliseconds
+
+var domain = document.getElementById("domain").getAttribute("value");
+function checkStatusHidden() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", domain + "/-/auth/status");
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function() {
+    if (this.readyState === HTTP_READY) {
+      if (this.status === HTTP_OK) {
+        return true;
+      }
+      else if (this.status === HTTP_CLIENT_ERROR) {
+        return false;
+      }
+      else {
+        return false;
+      }
+    }
+  };
+  xhr.send();
 }
-function checkStatusHidden()
-{
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', domain + '/-/auth/status');
-	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.onreadystatechange = function()
-    {
-        if(this.readyState == 4)
-        {
-        	if(this.status == 200)
-        		return true;
-        	else if(this.status == 401)
-        		return false;
-        	else
-        		return false;
-        }
-    };
-	xhr.send();
+
+function recaptchaCallback() {
+  document.getElementById("recaptcha_check_empty").value = 1;
+  var recaptchaData = grecaptcha.getResponse();
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", domain + "/-/auth/recaptcha");
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onload = function() {
+    console.log("response loaded");
+  };
+  xhr.send("data=" + recaptchaData);
+  var recaptchaWorked = setTimeout(checkStatusHidden(), RECAPTCHA_TIMEOUT);
+  if (recaptchaWorked) {
+    var toRemove = document.getElementById("recaptcha-form");
+    toRemove.parentNode.removeChild(toRemove);
+  }
 }
--- main-site/src/main/webapp/src/auth/auth.js
+++ main-site/src/main/webapp/src/auth/auth.js
@@ -1,91 +1,100 @@
-var domain = document.getElementById('domain').getAttribute('value');
-var redirect = document.getElementById('redirect').getAttribute('value');
+/* global gapi, grecaptcha */
+/* exported signOut, onSignIn, checkStatus, recaptchaCallback */
+/* eslint-env browser*/
+
+var HTTP_READY = 4;
+var HTTP_OK = 200;
+var HTTP_CLIENT_ERROR = 401;
+var RECAPTCHA_TIMEOUT = 2000; // in milliseconds
+
+var domain = document.getElementById("domain").getAttribute("value");
+var redirect = document.getElementById("redirect").getAttribute("value");
 console.log(redirect);
 
 function signOut() {
-	var auth2 = gapi.auth2.getAuthInstance();
-	auth2.signOut().then(function () {
-		console.log('User signed out.');
-	});
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function() {
+    console.log("User signed out.");
+  });
 }
-function onSignIn(googleUser)
-{
-	var id_token = googleUser.getAuthResponse().id_token;
-	var xhr = new XMLHttpRequest();
-	xhr.open('POST', domain + '/auth/google');
-	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	xhr.onload = function() {
-	  //console.log('response loaded');
-	};
-	xhr.send('id_token=' + id_token);
-}
-function checkStatus()
-{
-  document.getElementById('notice').innerHTML = 'Checking...';
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', domain + '/auth/status');
-	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.onreadystatechange = function()
-    {
-        if(this.readyState == 4)
-        {
-        	if(this.status == 200)
-        	{
-        		document.getElementById('notice').innerHTML = 'You Are Authorized';
-        		return true;
-        	}
-        	else if(this.status == 401)
-        		document.getElementById('notice').innerHTML = 'You Are Still Not Authorized';
-        	else
-        		document.getElementById('notice').innerHTML = 'Something Unexpected Happened';
-        }
-        else if(this.readyState == 1 || this.readyState == 2 || this.readyState == 3)
-        	document.getElementById('notice').innerHTML = 'Checking Status...';
-    };
-	xhr.send();
-}
-function recaptchaCallback()
-{
-	document.getElementById('recaptcha_check_empty').value = 1;
-	var recaptchaData = grecaptcha.getResponse();
-	var xhr = new XMLHttpRequest();
-	xhr.open('POST', domain + '/auth/recaptcha');
-	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	xhr.onload = function() {
-    var recaptchaWorked = setTimeout(checkStatusHidden(), 2000);
-  	if(recaptchaWorked)
-  	{
-  		var toRemove = document.getElementById('recaptcha-form');
-      toRemove.parentNode.removeChild(toRemove);
 
-      if(redirect != null)
-        window.location.replace(domain + redirect);
-  	}
-	};
-	xhr.send('data=' + recaptchaData);
+function onSignIn(googleUser) {
+  var id_token = googleUser.getAuthResponse().id_token;
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", domain + "/auth/google");
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onload = function() {
+    console.log("response loaded");
+  };
+  xhr.send("id_token=" + id_token);
 }
-function checkStatusHidden()
-{
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', domain + '/auth/status');
-	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.onreadystatechange = function()
-    {
-        if(this.readyState == 4)
-        {
-        	if(this.status == 200)
-        		return true;
-        	else if(this.status == 401)
-        		return false;
-        	else
-        		return false;
-        }
-    };
-    try {
-      xhr.send();
-    } catch (e) {
 
-    } finally {
-
+function checkStatus() {
+  document.getElementById("notice").innerHTML = "Checking...";
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", domain + "/auth/status");
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function() {
+    if (this.readyState === HTTP_READY) {
+      if (this.status === HTTP_OK) {
+        document.getElementById("notice").innerHTML = "You Are Authorized";
+        return true;
+      }
+      else if (this.status === HTTP_CLIENT_ERROR) {
+        document.getElementById("notice").innerHTML = "You Are Still Not Authorized";
+      }
+      else {
+        document.getElementById("notice").innerHTML = "Something Unexpected Happened";
+      }
     }
+    else if (this.readyState < HTTP_READY) {
+      document.getElementById("notice").innerHTML = "Checking Status...";
+    }
+  };
+  xhr.send();
+}
+
+function checkStatusHidden() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", domain + "/auth/status");
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function() {
+    if (this.readyState === HTTP_READY) {
+      if (this.status === HTTP_OK) {
+        return true;
+      }
+      else if (this.status === HTTP_CLIENT_ERROR) {
+        return false;
+      }
+      else {
+        return false;
+      }
+    }
+  };
+  try {
+    xhr.send();
+  }
+  catch (e) {
+    console.log(e);
+  }
+}
+
+function recaptchaCallback() {
+  document.getElementById("recaptcha_check_empty").value = 1;
+  var recaptcha_data = grecaptcha.getResponse();
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", domain + "/auth/recaptcha");
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onload = function() {
+    var recaptcha_worked = setTimeout(checkStatusHidden(), RECAPTCHA_TIMEOUT);
+    if (recaptcha_worked) {
+      var to_remove = document.getElementById("recaptcha-form");
+      to_remove.parentNode.removeChild(to_remove);
+
+      if (redirect !== null) {
+        window.location.replace(domain + redirect);
+      }
+    }
+  };
+  xhr.send("data=" + recaptcha_data);
 }
